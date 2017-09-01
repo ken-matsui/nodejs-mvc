fe52996034e066aaa9f47f89f292818512a9196d
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-09-02 08:54:11 +0900

    modify index.js

--- app/index.js
+++ app/index.js
@@ -1,17 +1,27 @@
 const EXPRESS = require('express');
+const PATH = require('path');
 const FS = require('fs');
+const BODY_PARSER = require('body-parser');
 const _ = require('underscore');
 
 const PORT = 8080;
 
 const APP = EXPRESS();
 
+// 描画エンジンにJadeを登録．
+APP.set('view engine', 'jade');
+// 静的ホスティングする．(これで静的ファイルはassets/以降のディレクトリ名で指定可能になる)
+APP.use(EXPRESS.static(PATH.join(__dirname, 'assets')));
+// BODY_PARSERの使用
+APP.use(BODY_PARSER.urlencoded({ extended: true }));
+APP.use(BODY_PARSER.json());
+
 // contrllerのファイル一覧を入れる(controllerの*.js)．(実行時処理)
 let aryControllFiles = [];
-FS.readdir("./controllers", (err_, files_) => {
+FS.readdir('./controllers', (err_, files_) => {
 	if (err_) throw err_;
 	_.each(files_, (file_, index_) => {
-		aryControllFiles.push(file_.replace(".js", ""));
+		aryControllFiles.push(file_.replace('.js', ''));
 	});
 });
 
@@ -28,23 +38,28 @@ function callController(url_, files_, dir_, req_, res_) {
 			callJs(`${dir_}/${file_}`, req_, res_);
 			return true;
 		} else if(url_[1] == "") {
-			callJs(dir_ + "/index", req_, res_);
+			callJs(dir_ + '/index', req_, res_);
 			return true;
 		}
 		return false;
 	});
 	if(_RETURN == false) {
-		callJs(dir_ + "/404", req_, res_);
+		callJs(dir_ + '/404', req_, res_);
 	}
 };
 
 // GETリクエストがあった場合の処理
-APP.get("/*", (req_, res_) => {
+APP.get('/*', (req_, res_) => {
 	let _url = req_.url.split("/");
-	callController(_url, aryControllFiles, "./controllers", req_, res_);
+	callController(_url, aryControllFiles, './controllers', req_, res_);
+});
+
+// POSTリクエストがあった場合の処理
+APP.post('/*', (req_, res_) => {
+	res_.json(req_.body);
 });
 
 APP.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}`);
-	console.log("Press Ctrl+C to quit.");
+	console.log('Press Ctrl+C to quit.');
 });
